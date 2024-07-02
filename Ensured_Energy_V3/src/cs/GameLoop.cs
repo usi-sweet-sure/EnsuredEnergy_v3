4d34df9fe8c805c27186007fd93ec607f862632f
Erwan Moreira <erwan.moreira@gmail.com>
2024-07-02 15:55:25 +0200

    PolicyManager

--- Ensured_Energy_V3/src/cs/GameLoop.cs
+++ Ensured_Energy_V3/src/cs/GameLoop.cs
@@ -40,8 +40,7 @@ public partial class GameLoop : Node2D {
 	// Retrieves the current resource estimates from the resource manager
 	public (Energy, Environment, Support) _GetResources() => RM._GetResources();
 
-	public void _OnPlayPressed() {
-	
-	}
+	public void _OnPlayPressed() {}
 
+	public void _ApplyEffect(Effect e) {}
 }
